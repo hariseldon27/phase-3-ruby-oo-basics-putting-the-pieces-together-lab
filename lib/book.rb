1b59e4fa187800750090379db16bd7addc6f4055
Jonathan Potter <jonathan@mrjonathanpotter.com>
2022-02-24 12:22:54 -0800

    commit tree 45327cb39e9699a941898de3d6dd28be03af715c

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,16 @@
+require 'pry'
+class Book
+    attr_accessor :title, :author, :page_count, :genre, :turn_page
 
+    def initialize(title)
+        @title = title
+        @author = "Agatha Christie"
+        @page_count = 272
+        @genre = "Mystery"
+    end
 
+    def turn_page
+        puts "Flipping the page...wow, you read fast!"
+    end
+end
+# binding.pry
--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,33 @@
 # Make your shoe class here!
+class Shoe
+    attr_writer :brand, :color, :size, :material, :condition
+    attr_reader :brand, :color, :size, :material, :condition
+
+    def initialize(brand)
+        @brand = brand
+    end
+
+    def color=(color)
+        @color = color
+    end
+
+    def size=(size)
+        @size = size
+    end
+
+    def material=(material)
+        @material = material
+    end
+
+    def condition=(condition)
+        @condition = condition
+    end
+    def cobble
+        if condition != "new"
+            self.condition = "new"
+            puts "Your shoe is as good as new!"
+        end
+        # puts "Your shoe is as good as new!"
+
+    end
+end
